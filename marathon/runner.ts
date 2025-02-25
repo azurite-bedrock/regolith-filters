cf3356f3ab232f253b0507d2a5897b69858adcb6
abysal <akashic.records.of.the.abyss@gmail.com>
2025-02-25 22:47:25 +0000

    Fixed another lack of braincells

--- marathon/runner.ts
+++ marathon/runner.ts
@@ -1,5 +1,5 @@
-import { walk } from "@std/fs";
-import { dirname } from "@std/path/dirname";
+import { walk } from '@std/fs';
+import { dirname } from '@std/path/dirname';
 
 interface Config {
     root_dir?: string;
@@ -22,25 +22,26 @@ function load_config() {
         return config;
     } catch {
         return {
-            root_dir: "./",
+            root_dir: './',
         };
     }
 }
 
 const config = load_config();
-const real_path = await Deno.realPath(config.root_dir!)
+const real_path = await Deno.realPath(config.root_dir!);
 
 const task_queue: Array<Generator> = new Array(0);
 let building = 0;
 
 Deno.chdir(config.root_dir!);
 
-for await (const file of walk("./", {
+for await (const file of walk('./', {
     includeDirs: false,
     followSymlinks: true,
     canonicalize: true,
+    exts: ['ts'],
 })) {
-    if (file.path.startsWith("data")) {
+    if (file.path.startsWith('data')) {
         continue;
     }
 
@@ -48,21 +49,21 @@ for await (const file of walk("./", {
 
     const file_data = await Deno.readTextFile(file.path);
 
-    if (!file_data.startsWith("await Deno.stdin.read(new Uint8Array(1));")) {
-        const data = "await Deno.stdin.read(new Uint8Array(1));\n" + file_data;
+    if (!file_data.startsWith('await Deno.stdin.read(new Uint8Array(1));')) {
+        const data = 'await Deno.stdin.read(new Uint8Array(1));\n' + file_data;
 
         await Deno.writeTextFile(file.path, data);
     }
 
     const process = new Deno.Command(Deno.execPath(), {
-        args: ["run", "--allow-all", await Deno.realPath(file.path)],
+        args: ['run', '--allow-all', await Deno.realPath(file.path)],
         env: {
             ROOT_DIR: real_path, // Set the environment variable here
         },
         cwd: dirname(file.path),
-        stdin: "piped",
-        stderr: "inherit",
-        stdout: "inherit",
+        stdin: 'piped',
+        stderr: 'inherit',
+        stdout: 'inherit',
     });
 
     task_queue.push({
@@ -84,7 +85,7 @@ while (building != 0 || task_queue.length != 0) {
     console.log(`Invoking ${cmd.name}`);
 
     const writer = cmd.process.stdin.getWriter();
-    await writer.write(new TextEncoder().encode("\n"));
+    await writer.write(new TextEncoder().encode('\n'));
     writer.close();
 
     const status = await cmd.process.status;
