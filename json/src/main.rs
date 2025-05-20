e5bcbc2b48d170676609a4631c35333be552660e
theaddon <theaddonn@gmail.com>
2025-05-20 15:47:35 +0200

    Add basic json filter

--- json/src/main.rs
+++ json/src/main.rs
@@ -1,9 +1,28 @@
-use clap::Args;
-use std::env::current_dir;
+use std::num::NonZero;
 
-#[derive(Args)]
-struct AppArgs {}
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+struct Config {
+    #[clap(
+        short,
+        long,
+        default_value_t = std::thread::available_parallelism().unwrap_or(unsafe {NonZero::new_unchecked(4)}).get()-1
+    )]
+    threads: usize,
+    #[clap(short, long, default_value_t = String::from("./"))]
+    dir: String,
+}
 
 fn main() {
-    current_dir()
+    let config = Config::parse();
+
+    let walkdir = WalkDir::new(config.dir)
+        .into_iter()
+        .filter_map(|entry| entry.ok());
+
+    for entry in walkdir {
+        println!("{:?}", entry.path())
+    }
 }
