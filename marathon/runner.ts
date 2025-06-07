694529da12b0efb729da71f1153474ad98efaef4
theaddon <theaddonn@gmail.com>
2025-06-07 11:54:33 +0200

    Add multi stage process

--- marathon/runner.ts
+++ marathon/runner.ts
@@ -14,76 +14,64 @@ interface Generator {
 function load_config() {
     try {
         const config = JSON.parse(Deno.args[0]) as Config;
-
         if (!config.root_dir) console.error('Settings object must contain a "root_dir"');
-
         return config;
     } catch {
-        return {
-            root_dir: './',
-        };
+        return { root_dir: './' };
     }
 }
 
 const config = load_config();
 const real_path = await Deno.realPath(config.root_dir!);
 
-const task_queue: Array<Generator> = new Array(0);
-let building = 0;
-
 Deno.chdir(config.root_dir!);
 
+// Step 1: List .ts files
+const tsFiles: string[] = [];
 for await (const file of walk('./', {
     includeDirs: false,
     followSymlinks: true,
     canonicalize: true,
     exts: ['ts'],
 })) {
-    if (file.path.startsWith('data')) continue;
+    const path = file.path;
+    if (path.startsWith('data') && !path.startsWith('data/marathon')) continue;
+    tsFiles.push(path);
+}
 
-    building++;
-
-    const file_data = await Deno.readTextFile(file.path);
-
+// Step 2: Run .ts files
+const task_queue: Array<Generator> = [];
+for (const filePath of tsFiles) {
+    const file_data = await Deno.readTextFile(filePath);
     if (!file_data.startsWith('await Deno.stdin.read(new Uint8Array(1));')) {
         const data = 'await Deno.stdin.read(new Uint8Array(1));\n' + file_data;
-
-        await Deno.writeTextFile(file.path, data);
+        await Deno.writeTextFile(filePath, data);
     }
-
     const process = new Deno.Command(Deno.execPath(), {
-        args: ['run', '--allow-all', await Deno.realPath(file.path)],
-        env: {
-            ROOT_DIR: real_path, // Set the environment variable here
-        },
-        cwd: dirname(file.path),
+        args: ['run', '--allow-all', await Deno.realPath(filePath)],
+        env: { ROOT_DIR: real_path },
+        cwd: dirname(filePath),
         stdin: 'piped',
         stderr: 'inherit',
         stdout: 'inherit',
     });
-
     task_queue.push({
-        path: file.path,
-        name: file.name,
+        path: filePath,
+        name: filePath.split('/').pop()!,
         process: process.spawn(),
     });
-
-    building--;
 }
 
-while (building != 0 || task_queue.length != 0) {
-    if (task_queue.length == 0) continue;
-
-    const cmd = task_queue.pop()!;
-
-    console.log(`Invoking ${cmd.name}`);
-
+for (const cmd of task_queue) {
+    console.info(`Invoking ${cmd.name}`);
     const writer = cmd.process.stdin.getWriter();
     await writer.write(new TextEncoder().encode('\n'));
     writer.close();
-
     const status = await cmd.process.status;
-    await Deno.remove(cmd.path);
-
     if (!status.success) Deno.exit(status.code);
+}
+
+// Step 3: Delete .ts files
+for (const filePath of tsFiles) {
+    await Deno.remove(filePath);
 }
